b3720297dbcd0a20ba7b948603f1953347b40dae
chiffon <chiffonlang@icloud.com>
2017-01-18 21:55:29 +0800

    add file converter function

--- R/hello.R
+++ R/hello.R
@@ -51,3 +51,32 @@ run_convert <- function(worker,text) {
 `[.cc_converter` <- function(ptr, code){
     return(run_convert(ptr, code))
 }
+
+
+#' @title convert_file
+#'
+#' @usage convert_file(input, output, type = ropencc::S2T, inputEncoding = 'UTF-8', ...)
+#'
+#' @param input input file name
+#' @param output output file name
+#' @param type one of T2S S2T S2TW HK2S S2HK S2TWP T2HK T2S T2TW TW2S TW2SP
+#' @param inputEncoding  encoding for input
+#' @param ... other parameter pass to readLines function
+#' @export
+
+convert_file = function(input,
+                        output,
+                        type = ropencc::S2T,
+                        inputEncoding = 'UTF-8',
+                        ...){
+    if(!file.exists(input)){
+        stop("no such file.")
+    }
+    if (file.exists(output)) {
+        warning("Thers is a file in the output path.")
+    }
+    strings = readLines(input, encoding = inputEncoding, ...)
+    outputStrings = converter(type)[strings]
+    writeLines(outputStrings, con = output, useBytes = T)
+    message("Finish writing!")
+}
